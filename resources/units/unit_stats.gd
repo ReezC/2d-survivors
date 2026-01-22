a25c2bd4cdcfc5991f4c1640e3b2427348480ff7
ReezC <reezc9586@gmail.com>
2026-01-22 12:54:41 +0800

    增加格挡相关属性

--- resources/units/unit_stats.gd
+++ resources/units/unit_stats.gd
@@ -10,9 +10,12 @@ enum UnitType {
 @export var 单位类型: UnitType 
 @export var 图标: Texture2D
 @export var 生命: int = 1
+@export var 攻击力: int = 1
 @export var 移动速度: float = 50
 @export var 幸运: int = 0
 @export var 暴击率: float = 0.05
 @export var 暴击时额外伤害: float = 0.5
+@export var 格挡率: float = 0.0
+@export var 格挡减伤倍率: float = 0.25
 @export var 护甲: int = 0
 @export var 闪避率: float = 0.0
